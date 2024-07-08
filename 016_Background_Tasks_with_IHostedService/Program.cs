f5d27b721c14fba1c35700e2c68094781beb8488
oneananda <8168596+oneananda@users.noreply.github.com>
2024-07-08 06:08:58 +0530

    Adding project for Background_Tasks_with_IHostedService

---
+++ 016_Background_Tasks_with_IHostedService/Program.cs
@@ -0,0 +1,40 @@
+
+using _016_Background_Tasks_with_IHostedService.Services;
+
+namespace _016_Background_Tasks_with_IHostedService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddHostedService<TimedHostedService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ 016_Background_Tasks_with_IHostedService/Services/TimedHostedService.cs
@@ -0,0 +1,46 @@
+﻿
+namespace _016_Background_Tasks_with_IHostedService.Services
+{
+    public class TimedHostedService : IHostedService, IDisposable
+    {
+        private int executionCount = 0;
+        private readonly ILogger<TimedHostedService> _logger;
+        private Timer? _timer;
+
+        public TimedHostedService(ILogger<TimedHostedService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Timed Hosted Service running.");
+
+            _timer = new Timer(callback: DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            // https://stackoverflow.com/a/9518256
+            var count = Interlocked.Increment(ref executionCount);
+
+            _logger.LogInformation("Timed Hosted Service is working. Count: {Count}", count);
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Timed Hosted Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
