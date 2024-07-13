124018b8da031a5c64f3a7fcaaade6f5ed95a311
oneananda <8168596+oneananda@users.noreply.github.com>
2024-07-13 22:16:29 +0530

    Adding additional changes

---
+++ 018-CQRS-Command-Query-Responsibility-Segregation/CQRS.Persistence/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CQRS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
