e79c8a1938db80da70556b3a8fbc14efbd492787
oneananda <8168596+oneananda@users.noreply.github.com>
2024-07-13 22:21:09 +0530

    Additional changes

---
+++ 018-CQRS-Command-Query-Responsibility-Segregation/CQRS.Application/Commands/CreateProductCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using CQRS.Domain.Entities;
+using CQRS.Infrastructure.Repositories;
+using System.Threading.Tasks;
+
+namespace CQRS.Application.Commands
+{
+    public class CreateProductCommandHandler
+    {
+        private readonly ProductRepository _repository;
+
+        public CreateProductCommandHandler(ProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateProductCommand command)
+        {
+            var product = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = command.Name,
+                Price = command.Price
+            };
+
+            await _repository.AddAsync(product);
+        }
+    }
+}
---
+++ 018-CQRS-Command-Query-Responsibility-Segregation/CQRS.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using CQRS.Domain.Entities;
+using CQRS.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRS.Infrastructure.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Product>> GetAllAsync() => await _context.Products.ToListAsync();
+
+        public async Task<Product> GetByIdAsync(Guid id) => await _context.Products.FindAsync(id);
+
+        public async Task AddAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
