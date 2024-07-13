c69eadee343d9e9aefd1d1a6978342baf5d206b1
oneananda <8168596+oneananda@users.noreply.github.com>
2024-07-13 22:27:33 +0530

    Additional changes

---
+++ 018-CQRS-Command-Query-Responsibility-Segregation/CQRS.Api/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CQRS.Application.Commands;
+using CQRS.Application.Queries;
+
+namespace CQRS.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly CreateProductCommandHandler _createHandler;
+        private readonly GetProductByIdQueryHandler _getHandler;
+
+        public ProductsController(CreateProductCommandHandler createHandler, GetProductByIdQueryHandler getHandler)
+        {
+            _createHandler = createHandler;
+            _getHandler = getHandler;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductCommand command)
+        {
+            await _createHandler.Handle(command);
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var query = new GetProductByIdQuery { Id = id };
+            var product = await _getHandler.Handle(query);
+            return Ok(product);
+        }
+    }
+}
---
+++ 018-CQRS-Command-Query-Responsibility-Segregation/CQRS.Application/Queries/GetProductByIdQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using CQRS.Domain.Entities;
+using CQRS.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Application.Queries
+{
+    public class GetProductByIdQueryHandler
+    {
+        private readonly ProductRepository _repository;
+
+        public GetProductByIdQueryHandler(ProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Product> Handle(GetProductByIdQuery query)
+        {
+            return await _repository.GetByIdAsync(query.Id);
+        }
+    }
+}
