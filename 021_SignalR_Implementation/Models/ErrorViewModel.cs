7a6d6b81fef3d75f6060637f45209a9547bf425a
oneananda <8168596+oneananda@users.noreply.github.com>
2024-07-18 06:09:19 +0530

    Adding site for ASP.NET Core SignalR Implementation

---
+++ 021_SignalR_Implementation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _021_SignalR_Implementation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
