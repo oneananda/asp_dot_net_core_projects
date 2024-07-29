0c2f558dfcd3db98195d4c9c17e7853551c10c44
oneananda <8168596+oneananda@users.noreply.github.com>
2024-07-29 20:57:42 +0530

    Adding ValidateAntiForgeryToken

---
+++ 022_Security_Best_Practices/Controllers/CSRFImplementationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _022_Security_Best_Practices.Controllers
+{
+    public class CSRFImplementationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Contact(string message)
+        {
+            // Process the message
+            return RedirectToAction("Index");
+        }
+    }
+}
