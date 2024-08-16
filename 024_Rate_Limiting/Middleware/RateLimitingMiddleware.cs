89f9df73c67e5966de786e88192709697e5fc7a3
oneananda <8168596+oneananda@users.noreply.github.com>
2024-08-16 16:20:58 +0530

    Adding InvokeAsync

--- 024_Rate_Limiting/Middleware/RateLimitingMiddleware.cs
+++ 024_Rate_Limiting/Middleware/RateLimitingMiddleware.cs
@@ -6,5 +6,24 @@ namespace _024_Rate_Limiting.Middleware
     {
         private readonly RequestDelegate _next;
         private static readonly MemoryCache memoryCache = new MemoryCache(new MemoryCacheOptions());
+
+        private const int MaxRequestsPerMinute = 60;
+        private const int TimeWindownInSeconds = 60;
+
+        public RateLimitingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var clientIp = context.Connection.RemoteIpAddress?.ToString();
+
+            if (clientIp == null)
+            {
+                await _next(context);
+                return;
+            }
+        }
     }
 }
