85e5c9755b5548fd3f55845f32bd36c340e8892e
oneananda <8168596+oneananda@users.noreply.github.com>
2024-08-16 16:49:55 +0530

    Adding IRequestDataService interface

---
+++ 024_Rate_Limiting/Interfaces/IRequestDataService.cs
@@ -0,0 +1,8 @@
+﻿namespace _024_Rate_Limiting.Interfaces
+{
+    public interface IRequestDataService
+    {
+        int GetRequestCount(string key);
+        void IncrementRequestCount(string key);
+    }
+}
