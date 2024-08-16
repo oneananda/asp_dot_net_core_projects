abd73d40bfd6b1cb04040eaba7769e59a17a8a3a
oneananda <8168596+oneananda@users.noreply.github.com>
2024-08-16 16:30:50 +0530

    Adding logic to MemoryCache

--- 024_Rate_Limiting/Middleware/RateLimitingMiddleware.cs
+++ 024_Rate_Limiting/Middleware/RateLimitingMiddleware.cs
@@ -5,7 +5,7 @@ namespace _024_Rate_Limiting.Middleware
     public class RateLimitingMiddleware
     {
         private readonly RequestDelegate _next;
-        private static readonly MemoryCache memoryCache = new MemoryCache(new MemoryCacheOptions());
+        private static readonly MemoryCache _cache = new MemoryCache(new MemoryCacheOptions());
 
         private const int MaxRequestsPerMinute = 60;
         private const int TimeWindownInSeconds = 60;
@@ -24,6 +24,25 @@ namespace _024_Rate_Limiting.Middleware
                 await _next(context);
                 return;
             }
+
+            var cacheKey = $"{clientIp}:{DateTime.UtcNow.Minute}";   
+
+            if(!_cache.TryGetValue(cacheKey, out int requestCount))
+            {
+                requestCount = 0;
+            }
+
+            if (requestCount >= MaxRequestsPerMinute)
+            {
+                context.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+                await context.Response.WriteAsync("Rate limit exceeded, Try again later!");
+                return;
+            }
+            requestCount++;
+
+            _cache.Set(cacheKey, requestCount, TimeSpan.FromSeconds(TimeWindownInSeconds));
+
+            await _next(context);
         }
     }
 }
