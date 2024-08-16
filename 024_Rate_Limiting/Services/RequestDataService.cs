29bca573098993aaba3c9b753fae7578f057d1ce
oneananda <8168596+oneananda@users.noreply.github.com>
2024-08-16 16:53:22 +0530

    Implementing IRequestDataService in RequestDataService

---
+++ 024_Rate_Limiting/Services/RequestDataService.cs
@@ -0,0 +1,21 @@
+﻿using _024_Rate_Limiting.Interfaces;
+using System.Collections.Concurrent;
+
+namespace _024_Rate_Limiting.Services
+{
+    public class RequestDataService : IRequestDataService
+    {
+        private readonly ConcurrentDictionary<string,int> _requestCounts = new();
+        public RequestDataService() { }
+        public int GetRequestCount(string key)
+        {
+           _requestCounts.TryGetValue(key, out var count);
+            return count;
+        }
+
+        public void IncrementRequestCount(string key)
+        {
+            _requestCounts.AddOrUpdate(key, 1, (k, v) => v + 1);
+        }
+    }
+}
