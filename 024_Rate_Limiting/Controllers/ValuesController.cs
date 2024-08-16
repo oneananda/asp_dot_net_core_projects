fdedd1b00302da68e6e0d791988c8d9e6e88ce8f
oneananda <8168596+oneananda@users.noreply.github.com>
2024-08-16 16:17:32 +0530

    Adding controller

---
+++ 024_Rate_Limiting/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _024_Rate_Limiting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
