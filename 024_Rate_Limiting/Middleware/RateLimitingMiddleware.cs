53987c70fb04ae532e88dd27df0b0994c3441208
oneananda <8168596+oneananda@users.noreply.github.com>
2024-08-16 16:17:44 +0530

    Adding RateLimitingMiddleware

---
+++ 024_Rate_Limiting/Middleware/RateLimitingMiddleware.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace _024_Rate_Limiting.Middleware
+{
+    public class RateLimitingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private static readonly MemoryCache memoryCache = new MemoryCache(new MemoryCacheOptions());
+    }
+}
