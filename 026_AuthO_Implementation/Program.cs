d559eaca145ea2c1257bccb77214ec2c991ff439
oneananda <8168596+oneananda@users.noreply.github.com>
2024-09-01 21:07:59 +0530

    _026_AuthO_Implementation changes

--- 026_AuthO_Implementation/Program.cs
+++ 026_AuthO_Implementation/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 namespace _026_AuthO_Implementation
 {
     public class Program
@@ -10,9 +13,66 @@ namespace _026_AuthO_Implementation
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+
+
+
+
+            // Configure Auth0 JWT Bearer Authentication
+            var auth0Settings = builder.Configuration.GetSection("Auth0");
+            var domain = auth0Settings["Domain"];
+            var audience = auth0Settings["Audience"];
+
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.Authority = $"https://{domain}/";
+                options.Audience = audience;
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidAudience = audience,
+                    ValidIssuer = $"https://{domain}/",
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true
+                };
+                options.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        Console.WriteLine($"Token failed validation: {context.Exception.Message}");
+                        return Task.CompletedTask;
+                    },
+                    OnTokenValidated = context =>
+                    {
+                        Console.WriteLine("Token validated successfully.");
+                        return Task.CompletedTask;
+                    }
+                };
+
+            });
+
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
+                    });
+            });
 
             var app = builder.Build();
 
@@ -23,7 +83,11 @@ namespace _026_AuthO_Implementation
                 app.UseSwaggerUI();
             }
 
-            //app.UseHttpsRedirection();
+            app.UseCors("AllowAll");
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
