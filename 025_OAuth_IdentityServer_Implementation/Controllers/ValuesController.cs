2ed75b7aab8cf27621057ac683a9ec832a983e8a
oneananda <8168596+oneananda@users.noreply.github.com>
2024-09-01 15:11:20 +0530

    Identity server changes

---
+++ 025_OAuth_IdentityServer_Implementation/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _025_OAuth_IdentityServer_Implementation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+    }
+}
--- 025_OAuth_IdentityServer_Implementation/Program.cs
+++ 025_OAuth_IdentityServer_Implementation/Program.cs
@@ -1,4 +1,7 @@
 
+using Duende.IdentityServer.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 namespace _025_OAuth_IdentityServer_Implementation
 {
     public class Program
@@ -7,6 +10,42 @@ namespace _025_OAuth_IdentityServer_Implementation
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // Create Identity server
+
+            builder.Services.AddIdentityServer(options =>
+            {
+                options.EmitStaticAudienceClaim = true;
+            })
+            .AddInMemoryClients(new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "clientId",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = {new Secret("".Sha256())},
+                    AllowedScopes = {"api1" }
+                }
+            })
+            .AddInMemoryApiScopes(new List<ApiScope>
+            {
+                new ApiScope("api1", "My API")
+            });
+
+            // Add authentication services
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = "https://localhost:5278"; // IdentityServer URL
+                    options.Audience = "api1";
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true
+                    };
+                });
+
             // Add services to the container.
 
             builder.Services.AddControllers();
@@ -14,6 +53,8 @@ namespace _025_OAuth_IdentityServer_Implementation
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Logging.AddConsole();
+            
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -22,6 +63,9 @@ namespace _025_OAuth_IdentityServer_Implementation
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            app.UseIdentityServer();
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
