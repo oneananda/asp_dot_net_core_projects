3d3aa77fa12fdba4646ccac2f57d425d6c76ad10
oneananda <anandathirumurugan@gmail.com>
2024-10-01 21:47:06 +0530

    Adding CircuitBreaker class

---
+++ 028_Circuit_Breaker_Pattern/Models/CircuitBreaker.cs
@@ -0,0 +1,12 @@
+﻿namespace _028_Circuit_Breaker_Pattern.Models
+{
+    public class CircuitBreaker
+    {
+        private enum State
+        {
+            Closed,
+            Open,
+            HalfOpen
+        }
+    }
+}
