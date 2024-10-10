a1b9048f1b1f2ff56435215e037f29a08712de4f
oneananda <anandathirumurugan@gmail.com>
2024-10-10 16:51:32 +0530

    Adding constructor for CircuitBreaker

--- 028_Circuit_Breaker_Pattern/Models/CircuitBreaker.cs
+++ 028_Circuit_Breaker_Pattern/Models/CircuitBreaker.cs
@@ -8,5 +8,17 @@
             Open,
             HalfOpen
         }
+
+        private State _state = State.Closed;
+        private int _failureCount = 0;
+        private readonly int _failureTheshold;
+        private readonly TimeSpan _openTimeOut;
+        private DateTime _lastFailureTime;
+
+        private CircuitBreaker(int failureThreshold, TimeSpan openTimeOut)
+        {
+            _failureTheshold = failureThreshold;
+            _openTimeOut = openTimeOut;
+        }
     }
 }
