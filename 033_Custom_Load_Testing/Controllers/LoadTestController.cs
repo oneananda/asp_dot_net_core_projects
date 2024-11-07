9984dccfcd1b323a64e34438a6b62f3231617662
oneananda <anandathirumurugan@gmail.com>
2024-11-07 22:58:18 +0530

    Adding LoadTestController

---
+++ 033_Custom_Load_Testing/Controllers/LoadTestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _033_Custom_Load_Testing.Controllers
+{
+    public class LoadTestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
