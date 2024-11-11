8f9e7eb453f5ef4f447a21d882d86fee78c20826
oneananda <anandathirumurugan@gmail.com>
2024-11-11 21:39:44 +0530

    Adding GeneralController

---
+++ 032_Integration_Tests_ASP_Dot_NET_Core/Controllers/GeneralController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _032_Integration_Tests_ASP_Dot_NET_Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeneralController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("General Get!");
+        }
+    }
+}
