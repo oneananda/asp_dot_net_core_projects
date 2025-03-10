3220c6d008fab7710d275fc4b1411bbba1ca13b2
oneananda <anandathirumurugan@gmail.com>
2025-03-10 18:08:05 +0530

    Adding GetInstanceId

--- 034_AddTransient_DeepDive/Services/BasicService.cs
+++ 034_AddTransient_DeepDive/Services/BasicService.cs
@@ -1,6 +1,25 @@
-﻿namespace _034_AddTransient_DeepDive.Services
+﻿using _034_AddTransient_DeepDive.DTOs;
+using _034_AddTransient_DeepDive.Models;
+using _034_AddTransient_DeepDive.Services.Interfaces;
+
+namespace _034_AddTransient_DeepDive.Services
 {
-    public class BasicService
+    public class BasicService : IBasicService
     {
+        private readonly Guid _instanceId;
+
+        public BasicService()
+        {
+            _instanceId = Guid.NewGuid();
+        }
+        public Basic GetBasicDetails(int Id)
+        {
+            return new Basic { Id = Id, Name = Guid.NewGuid().ToString() };
+        }
+
+        public Guid GetInstanceId()
+        {
+            return _instanceId;
+        }
     }
 }
