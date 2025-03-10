5777748e8770d4765e57f4c6f7a56679c146162b
oneananda <anandathirumurugan@gmail.com>
2025-03-10 18:03:29 +0530

    GetBasicDetails

--- 034_AddTransient_DeepDive/Services/Interfaces/IBasicService.cs
+++ 034_AddTransient_DeepDive/Services/Interfaces/IBasicService.cs
@@ -1,8 +1,11 @@
-﻿namespace _034_AddTransient_DeepDive.Services.Interfaces
+﻿using _034_AddTransient_DeepDive.Models;
+
+namespace _034_AddTransient_DeepDive.Services.Interfaces
 {
     public interface IBasicService
     {
         Guid GetInstanceId();
 
+        Basic GetBasicDetails(int Id);
     }
 }
