d9d1241f6abba02ebf6be18c759bc71a63c5932d
oneananda <anandathirumurugan@gmail.com>
2025-03-11 20:40:30 +0530

    Adding IActionResult Get()

---
+++ 035_NancyFX_To_Core_Migration/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _035_NancyFX_To_Core_Migration.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from ASP.NET Core!");
+        }
+    }
+}
