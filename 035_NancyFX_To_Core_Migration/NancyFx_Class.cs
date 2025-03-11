edc1d7871f04070965eb92ed2be2fb3a1fe89ca4
oneananda <anandathirumurugan@gmail.com>
2025-03-11 20:38:50 +0530

    Adding NancyFx_Class

---
+++ 035_NancyFX_To_Core_Migration/NancyFx_Class.cs
@@ -0,0 +1,14 @@
+﻿using Nancy;
+
+namespace _035_NancyFX_To_Core_Migration
+{
+    public class NancyFx_Class : NancyModule
+    {
+        public NancyFx_Class()
+        {
+            Get("/", args => "Hello from NancyFX!");
+
+            Get("/greet/{name}", args => $"Hello, {args.name}!");
+        }
+    }
+}
