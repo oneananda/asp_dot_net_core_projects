041011c80991119276cf2b8808b4c10eb757ab4a
oneananda <anandathirumurugan@gmail.com>
2025-03-12 20:38:04 +0530

    Adding ServiceB

---
+++ 036_AddScoped_DeepDive/Services/ServiceB.cs
@@ -0,0 +1,18 @@
+﻿using _036_AddScoped_DeepDive.Interfaces;
+
+namespace _036_AddScoped_DeepDive.Services
+{
+    public class ServiceB : IServiceB
+    {
+        private readonly IRequestTracker _requestTracker;
+        public ServiceB(IRequestTracker requestTracker)
+        {
+            _requestTracker = requestTracker;
+        }
+
+        public void PrintRequestId()
+        {
+            Console.WriteLine($"ServiceB: {_requestTracker.RequestId}");
+        }
+    }
+}
