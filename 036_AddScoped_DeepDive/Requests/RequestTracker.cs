cb9a36d70687b454cdce4a845b661dfabfe5b14f
oneananda <anandathirumurugan@gmail.com>
2025-03-12 20:19:19 +0530

    Adding RequestTracker

---
+++ 036_AddScoped_DeepDive/Requests/RequestTracker.cs
@@ -0,0 +1,9 @@
+﻿using _036_AddScoped_DeepDive.Interfaces;
+
+namespace _036_AddScoped_DeepDive.Requests
+{
+    public class RequestTracker : IRequestTracker
+    {
+        Guid IRequestTracker.RequestId => Guid.NewGuid();
+    }
+}
