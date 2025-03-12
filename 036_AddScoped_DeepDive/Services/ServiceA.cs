5726ae3b64c1d50dbb57446481369d665f16410c
oneananda <anandathirumurugan@gmail.com>
2025-03-12 20:37:35 +0530

    Adding ServiceA

---
+++ 036_AddScoped_DeepDive/Services/ServiceA.cs
@@ -0,0 +1,17 @@
+﻿using _036_AddScoped_DeepDive.Interfaces;
+
+namespace _036_AddScoped_DeepDive.Services
+{
+    public class ServiceA : IServiceA
+    {
+        private readonly IRequestTracker _requestTracker;
+        public ServiceA(IRequestTracker requestTracker)
+        {
+            _requestTracker = requestTracker;
+        }
+        public void PrintRequestId()
+        {
+            Console.WriteLine($"ServiceA: {_requestTracker.RequestId}");
+        }
+    }
+}
