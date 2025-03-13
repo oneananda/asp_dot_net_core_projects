ac9d06d536ff0511f112d1d36205c91b23864eaf
oneananda <anandathirumurugan@gmail.com>
2025-03-13 19:10:33 +0530

    Example2Controller

---
+++ 036_AddScoped_DeepDive/Controllers/Example2Controller.cs
@@ -0,0 +1,29 @@
+﻿using _036_AddScoped_DeepDive.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _036_AddScoped_DeepDive.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Example2Controller : ControllerBase
+    {
+        private readonly ExampleService2 _service1;
+        private readonly ExampleService2 _service2;
+
+        public Example2Controller(ExampleService2 service1, ExampleService2 service2)
+        {
+            _service1 = service1;
+            _service2 = service2;
+        }
+
+        [HttpGet("scoped-demo")]
+        public IActionResult Get()
+        {
+            Console.WriteLine($"Scoped Service ID1: {_service1.Id}");
+            Console.WriteLine($"Scoped Service ID2: {_service2.Id}");
+
+            return Ok(new { Service1Id = _service1.Id, Service12d = _service2.Id });
+        }
+    }
+}
---
+++ 036_AddScoped_DeepDive/Services/ExampleService2.cs
@@ -0,0 +1,14 @@
+﻿namespace _036_AddScoped_DeepDive.Services
+{
+    public class ExampleService2 : IDisposable
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+
+        public Guid GeneratedId { set; get; }
+        public void Dispose()
+        {
+            GeneratedId = Id;
+            Console.WriteLine($"Disposed {Id}");
+        }
+    }
+}
