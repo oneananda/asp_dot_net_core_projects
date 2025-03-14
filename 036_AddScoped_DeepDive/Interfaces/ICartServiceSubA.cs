f9e1013e2d76477920ad37c8d1d11faed959e51b
oneananda <anandathirumurugan@gmail.com>
2025-03-14 06:07:04 +0530

    Adding CartServiceSubA

---
+++ 036_AddScoped_DeepDive/Interfaces/ICartServiceSubA.cs
@@ -0,0 +1,7 @@
+﻿namespace _036_AddScoped_DeepDive.Interfaces
+{
+    public interface ICartServiceSubA
+    {
+        void AddToCart(string item);
+    }
+}
---
+++ 036_AddScoped_DeepDive/Services/CartServiceSubA.cs
@@ -0,0 +1,18 @@
+﻿using _036_AddScoped_DeepDive.Interfaces;
+
+namespace _036_AddScoped_DeepDive.Services
+{
+    public class CartServiceSubA : ICartServiceSubA
+    {
+        private readonly ICartService _cartService;
+
+        public CartServiceSubA(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+        public void AddToCart(string item)
+        {
+            _cartService.AddItem($"CartServiceSubA added: {item}");
+        }
+    }
+}
