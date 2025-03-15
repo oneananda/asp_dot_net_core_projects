4b5deef0edc86e918d3576d0ddb3f19e0990425e
oneananda <anandathirumurugan@gmail.com>
2025-03-15 10:43:28 +0530

    Adding DBContextExample

---
+++ 036_AddScoped_DeepDive/Models/DBContextExample/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace _036_AddScoped_DeepDive.Models.DBContextExample
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ 036_AddScoped_DeepDive/Services/DBContextExample/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using _036_AddScoped_DeepDive.Models.DBContextExample;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace _036_AddScoped_DeepDive.Services.DBContextExample
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
