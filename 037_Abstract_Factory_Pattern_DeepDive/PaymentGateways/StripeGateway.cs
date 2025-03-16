47c13ca52aafd5ac83e8f23f153871d0451ee49d
oneananda <anandathirumurugan@gmail.com>
2025-03-16 06:48:02 +0530

    StripeGateway

---
+++ 037_Abstract_Factory_Pattern_DeepDive/PaymentGateways/StripeGateway.cs
@@ -0,0 +1,12 @@
+﻿namespace _037_Abstract_Factory_Pattern_DeepDive.PaymentGateways
+{
+    public class StripeGateway : IPaymentGateway
+    {
+        public async Task<string> ProcessPayment(decimal amount)
+        {
+            // Implement Stripe API logic
+            await Task.Delay(100); // Simulate API call
+            return $"Processed {amount} USD via Stripe.";
+        }
+    }
+}
