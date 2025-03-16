3556ef8b44e9142d8d90b33944b29571a913d403
oneananda <anandathirumurugan@gmail.com>
2025-03-16 06:48:56 +0530

    Abstract Factory Interface

---
+++ 037_Abstract_Factory_Pattern_DeepDive/PaymentGateways/IPaymentGatewayFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace _037_Abstract_Factory_Pattern_DeepDive.PaymentGateways
+{
+    public interface IPaymentGatewayFactory
+    {
+        IPaymentGateway CreatePaymentGateway(string provider);
+    }
+}
