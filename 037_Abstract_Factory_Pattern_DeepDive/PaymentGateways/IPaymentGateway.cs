e00e4561c7f81dff6c955bc6452426cd8d967706
oneananda <anandathirumurugan@gmail.com>
2025-03-16 06:47:02 +0530

    Implement Concrete Products - PayPalGateway

---
+++ 037_Abstract_Factory_Pattern_DeepDive/PaymentGateways/IPaymentGateway.cs
@@ -0,0 +1,7 @@
+﻿namespace _037_Abstract_Factory_Pattern_DeepDive.PaymentGateways
+{
+    public interface IPaymentGateway
+    {
+        Task<string> ProcessPayment(decimal amount);
+    }
+}
---
+++ 037_Abstract_Factory_Pattern_DeepDive/PaymentGateways/PayPalGateway.cs
@@ -0,0 +1,13 @@
+﻿
+namespace _037_Abstract_Factory_Pattern_DeepDive.PaymentGateways
+{
+    public class PayPalGateway : IPaymentGateway
+    {
+        public async Task<string> ProcessPayment(decimal amount)
+        {
+            // Implement PayPal API logic
+            await Task.Delay(100); // Simulate API call
+            return $"Processed {amount} USD via PayPal.";
+        }
+    }
+}
