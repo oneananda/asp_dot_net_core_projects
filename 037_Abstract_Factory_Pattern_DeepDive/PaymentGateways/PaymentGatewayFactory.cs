73dbf3d62d0866774aee839756cbcb98e373f505
oneananda <anandathirumurugan@gmail.com>
2025-03-16 06:51:54 +0530

    Adding PaymentGatewayFactory

---
+++ 037_Abstract_Factory_Pattern_DeepDive/PaymentGateways/PaymentGatewayFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace _037_Abstract_Factory_Pattern_DeepDive.PaymentGateways
+{
+    // PaymentGatewayFactory.cs
+    public class PaymentGatewayFactory : IPaymentGatewayFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public PaymentGatewayFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public IPaymentGateway CreatePaymentGateway(string provider)
+        {
+            return provider.ToLower() switch
+            {
+                "paypal" => _serviceProvider.GetService<PayPalGateway>(),
+                "stripe" => _serviceProvider.GetService<StripeGateway>(),
+                _ => throw new ArgumentException($"Unsupported provider: {provider}")
+            };
+        }
+    }
+
+}
