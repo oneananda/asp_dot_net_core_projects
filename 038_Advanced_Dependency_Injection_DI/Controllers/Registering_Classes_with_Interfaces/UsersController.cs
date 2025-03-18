556d434c25d12f5da5529eeca0748b4663cdc88e
oneananda <anandathirumurugan@gmail.com>
2025-03-18 19:05:19 +0530

    Adding UsersController

---
+++ 038_Advanced_Dependency_Injection_DI/Controllers/Registering_Classes_with_Interfaces/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using _038_Advanced_Dependency_Injection_DI.Registering_Classes_with_Interfaces.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _038_Advanced_Dependency_Injection_DI.Controllers.Registering_Classes_with_Interfaces
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public IActionResult Create(string userName)
+        {
+            _userService.CreateUser(userName);
+            return Ok();
+        }
+    }
+}
