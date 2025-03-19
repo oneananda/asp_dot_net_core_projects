42ee9542475b6665928d8ef31835b774ddb7b2c6
oneananda <anandathirumurugan@gmail.com>
2025-03-19 20:39:31 +0530

    Adding EmailController

---
+++ 038_Advanced_Dependency_Injection_DI/Controllers/Registering_Classes_with_Interfaces/EmailController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _038_Advanced_Dependency_Injection_DI.Controllers.Registering_Classes_with_Interfaces
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmailController : ControllerBase
+    {
+        private readonly EmailSender _emailSender;
+
+        public EmailController(EmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        [HttpPost]
+        public IActionResult Send(string email, string message)
+        {
+            _emailSender.SendEmail(email, message);
+            return Ok();
+        }
+    }
+
+}
