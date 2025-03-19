9cadd897d688e3f9900099fb26ef8e907775cd0f
oneananda <anandathirumurugan@gmail.com>
2025-03-19 21:52:31 +0530

    StripeGateway

---
+++ 038_Advanced_Dependency_Injection_DI/Registering_Multiple_Implementations_Single_Interface/PaypalGateway.cs
@@ -0,0 +1,7 @@
+﻿namespace _038_Advanced_Dependency_Injection_DI.Registering_Multiple_Implementations_Single_Interface
+{
+    public class PaypalGateway : IPaymentGateway
+    {
+        public void ProcessPayment(decimal amount) { /* PayPal logic */ }
+    }
+}
---
+++ 038_Advanced_Dependency_Injection_DI/Registering_Multiple_Implementations_Single_Interface/StripeGateway.cs
@@ -0,0 +1,7 @@
+﻿namespace _038_Advanced_Dependency_Injection_DI.Registering_Multiple_Implementations_Single_Interface
+{
+    public class StripeGateway : IPaymentGateway
+    {
+        public void ProcessPayment(decimal amount) { /* Stripe logic */ }
+    }
+}
