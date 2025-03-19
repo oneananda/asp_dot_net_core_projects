16dfa6b71c88cf4f86a78b7ee30885c4c507e908
oneananda <anandathirumurugan@gmail.com>
2025-03-19 09:02:22 +0530

    Adding SaveChangesAsync

--- 038_Advanced_Dependency_Injection_DI/Controllers/Registering_Classes_with_Interfaces/UsersController.cs
+++ 038_Advanced_Dependency_Injection_DI/Controllers/Registering_Classes_with_Interfaces/UsersController.cs
@@ -1,7 +1,9 @@
-﻿using _038_Advanced_Dependency_Injection_DI.Registering_Classes_with_Interfaces.Interfaces;
+﻿using _038_Advanced_Dependency_Injection_DI.Common;
+using _038_Advanced_Dependency_Injection_DI.Registering_Classes_with_Interfaces.Interfaces;
 using _038_Advanced_Dependency_Injection_DI.Registering_Classes_with_Interfaces.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace _038_Advanced_Dependency_Injection_DI.Controllers.Registering_Classes_with_Interfaces
 {
@@ -9,17 +11,20 @@ namespace _038_Advanced_Dependency_Injection_DI.Controllers.Registering_Classes_
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly RCIAppDbContext _context;
         private readonly IUserService _userService;
 
-        public UsersController(IUserService userService)
+        public UsersController(RCIAppDbContext rCIAppDbContext, IUserService userService)
         {
             _userService = userService;
+            _context = rCIAppDbContext;
         }
 
         [HttpPost]
         public IActionResult Create(User user)
         {
             _userService.CreateUser(user);
+            _context.SaveChangesAsync();
             return Ok(new { AddedUser = user, AllUsers = _userService.GetUsersAsync() });
         }
     }
