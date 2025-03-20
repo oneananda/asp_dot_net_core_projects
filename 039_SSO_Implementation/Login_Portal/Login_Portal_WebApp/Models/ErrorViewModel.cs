2ab9c58e2fef7c00157ec9d0859083f350faf7b4
oneananda <anandathirumurugan@gmail.com>
2025-03-21 03:45:30 +0530

    Adding Login_Portal_WebApp

---
+++ 039_SSO_Implementation/Login_Portal/Login_Portal_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Login_Portal_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
