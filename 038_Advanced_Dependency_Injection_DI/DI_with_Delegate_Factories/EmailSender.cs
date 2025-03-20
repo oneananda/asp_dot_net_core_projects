000d9a1c6ada8a3aafaebf21f98ef6dbb4abf9be
oneananda <anandathirumurugan@gmail.com>
2025-03-20 20:53:05 +0530

    Adding EmailSender

---
+++ 038_Advanced_Dependency_Injection_DI/DI_with_Delegate_Factories/EmailSender.cs
@@ -0,0 +1,18 @@
+﻿namespace _038_Advanced_Dependency_Injection_DI.DI_with_Delegate_Factories
+{
+    public class EmailSender
+    {
+        private readonly string _smtpServer;
+
+        public EmailSender(string smtpServer)
+        {
+            _smtpServer = smtpServer;
+        }
+
+        public void Send(string email, string message)
+        {
+            // send email logic
+        }
+    }
+
+}
