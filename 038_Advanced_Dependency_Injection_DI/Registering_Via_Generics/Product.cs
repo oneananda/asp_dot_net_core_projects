5cf8590e08c00cdac67c3bbf5a8771bd58b6b4ad
oneananda <anandathirumurugan@gmail.com>
2025-03-20 20:40:32 +0530

    Adding Product

---
+++ 038_Advanced_Dependency_Injection_DI/Registering_Via_Generics/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace _038_Advanced_Dependency_Injection_DI.Registering_Via_Generics
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
