5f158d4808108be66680bbec8d9ee64467826aa2
oneananda <anandathirumurugan@gmail.com>
2025-03-20 20:39:34 +0530

    ProductsController

---
+++ 038_Advanced_Dependency_Injection_DI/Controllers/Registering_Via_Generics/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using _038_Advanced_Dependency_Injection_DI.Registering_Via_Generics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _038_Advanced_Dependency_Injection_DI.Controllers.Registering_Via_Generics
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IRepository<Product> _productRepo;
+
+        public ProductsController(IRepository<Product> productRepo)
+        {
+            _productRepo = productRepo;
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            _productRepo.Add(product);
+            return Ok();
+        }
+    }
+}
---
+++ 038_Advanced_Dependency_Injection_DI/Registering_Via_Generics/Repository.cs
@@ -0,0 +1,11 @@
+﻿namespace _038_Advanced_Dependency_Injection_DI.Registering_Via_Generics
+{
+    public class Repository<T> : IRepository<T>
+    {
+        public void Add(T entity)
+        {
+            // Generic repository logic
+        }
+    }
+
+}
