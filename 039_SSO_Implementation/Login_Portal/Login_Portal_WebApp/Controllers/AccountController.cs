7076660a491d0c145113ae956aa696aa78131957
oneananda <anandathirumurugan@gmail.com>
2025-03-21 07:04:48 +0530

    Adding AccountController

---
+++ 039_SSO_Implementation/Login_Portal/Login_Portal_WebApp/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Login_Portal_WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
