10aa671486d12c5d8ddd4273370334f603980462
oneananda <anandathirumurugan@gmail.com>
2025-03-23 21:26:23 +0530

    Checking IsValidated

--- 039_SSO_Implementation/Login_Portal/Login_Portal_WebApp/Controllers/SSOConnect.cs
+++ 039_SSO_Implementation/Login_Portal/Login_Portal_WebApp/Controllers/SSOConnect.cs
@@ -6,6 +6,14 @@ namespace Login_Portal_WebApp.Controllers
     {
         public IActionResult Account()
         {
+            string isValidatedStr = HttpContext.Session.GetString("IsValidated");
+            bool isValidated = false;
+
+            if (isValidatedStr != null)
+            {
+                // Optionally convert the string to a boolean if stored that way
+                bool.TryParse(isValidatedStr, out isValidated);
+            }
             return View();
         }
     }
