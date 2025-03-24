308f24a8388764f886ec102520a7f975a61f167d
oneananda <anandathirumurugan@gmail.com>
2025-03-24 20:51:38 +0530

    Adding new project Main Portal WebApp

---
+++ 039_SSO_Implementation/Main_Portal_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Main_Portal_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
