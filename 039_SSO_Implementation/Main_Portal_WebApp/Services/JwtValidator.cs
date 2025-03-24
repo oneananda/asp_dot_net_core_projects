fb0f1ee86299b644c0fcb01e7f480b5f12be3013
oneananda <anandathirumurugan@gmail.com>
2025-03-24 21:04:36 +0530

    JwtValidator - ValidateToken

---
+++ 039_SSO_Implementation/Main_Portal_WebApp/Services/JwtValidator.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Main_Portal_WebApp.Services
+{
+    public class JwtValidator
+    {
+        public static ClaimsPrincipal ValidateToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes("MyVeryStrongSecretKey123_MyVeryStrongSecretKey123_MyVeryStrongSecretKey123_sjhbs"); // must match Issuer
+
+            var validationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ClockSkew = TimeSpan.Zero
+            };
+
+            try
+            {
+                SecurityToken validatedToken;
+                var principal = tokenHandler.ValidateToken(token, validationParameters, out validatedToken);
+                return principal;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+
+}
