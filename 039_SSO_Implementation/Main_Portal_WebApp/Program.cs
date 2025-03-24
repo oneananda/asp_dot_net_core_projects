9326e772fead78dd2b604c2910448a7acec8b71b
oneananda <anandathirumurugan@gmail.com>
2025-03-24 21:07:16 +0530

    Adding AddSession

--- 039_SSO_Implementation/Main_Portal_WebApp/Program.cs
+++ 039_SSO_Implementation/Main_Portal_WebApp/Program.cs
@@ -9,6 +9,13 @@ namespace Main_Portal_WebApp
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30); // Session timeout
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
