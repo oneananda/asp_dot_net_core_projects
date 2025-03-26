b28361a96330c657e33e7930b1c05568fe98743a
oneananda <anandathirumurugan@gmail.com>
2025-03-26 20:53:37 +0530

    Adding Product class

---
+++ 040_Entity_Framework_Implementation/EF_Implementation/Models/Product.cs
@@ -0,0 +1,9 @@
+// Models/Product.cs
+namespace EF_Implementation.Models;
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
+
