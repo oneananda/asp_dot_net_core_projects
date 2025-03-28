9f31001b0636ec9749c764c72d12f8652f525775
oneananda <anandathirumurugan@gmail.com>
2025-03-28 05:44:08 +0530

    User model

---
+++ 041_MultiTenant_Architecture_Implementation/MultiTenant_Architecture_Implementation/Database_Per_Tenant/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Database_Per_Tenant.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database_Per_Tenant.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<User> Products { get; set; }
+    }
+}
---
+++ 041_MultiTenant_Architecture_Implementation/MultiTenant_Architecture_Implementation/Database_Per_Tenant/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Principal;
+
+namespace Database_Per_Tenant.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
