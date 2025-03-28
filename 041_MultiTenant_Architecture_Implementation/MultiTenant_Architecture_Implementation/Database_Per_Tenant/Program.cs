a60ff541f2dfede87269459f41797b857b0ad691
oneananda <anandathirumurugan@gmail.com>
2025-03-28 05:39:51 +0530

    Adding AddDbContext

--- 041_MultiTenant_Architecture_Implementation/MultiTenant_Architecture_Implementation/Database_Per_Tenant/Program.cs
+++ 041_MultiTenant_Architecture_Implementation/MultiTenant_Architecture_Implementation/Database_Per_Tenant/Program.cs
@@ -1,3 +1,6 @@
+using Database_Per_Tenant.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Database_Per_Tenant
 {
     public class Program
@@ -8,6 +11,9 @@ namespace Database_Per_Tenant
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
