67eac4b61f2223bc538893d8eee9eb2ac76d840d
oneananda <anandathirumurugan@gmail.com>
2025-03-30 20:16:16 +0530

    Adding Client class

---
+++ 041_MultiTenant_Architecture_Implementation/MultiTenant_Architecture_Implementation/Database_Per_Tenant/Models/Client.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Database_Per_Tenant.Models
+{
+    public class Client
+    {
+        [Key]
+        public Guid ClientId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string ClientName { get; set; }
+
+        // The connection string specific to the tenant's database.
+        [Required]
+        public string ConnectionString { get; set; }
+
+        // Track when the client record was created.
+        public DateTime CreatedDate { get; set; }
+
+        public Client()
+        {
+            // Generate a new unique identifier for the tenant.
+            ClientId = Guid.NewGuid();
+            CreatedDate = DateTime.UtcNow;
+        }
+    }
+}
