60cad9ea4c79dae0536b619c8faaac23fd685d62
oneananda <anandathirumurugan@gmail.com>
2025-04-03 16:31:41 +0530

    Adding ConfigController

---
+++ 042_AddSingleton_Deep_Dive/Controllers/ConfigController.cs
@@ -0,0 +1,24 @@
+﻿using _042_AddSingleton_Deep_Dive.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _042_AddSingleton_Deep_Dive.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        private readonly AppSettings _settings;
+
+        public ConfigController(AppSettings settings)
+        {
+            _settings = settings;
+        }
+
+        [HttpGet("name")]
+        public IActionResult GetAppName()
+        {
+            return Ok(new { AppName = _settings.AppName });
+        }
+    }
+}
