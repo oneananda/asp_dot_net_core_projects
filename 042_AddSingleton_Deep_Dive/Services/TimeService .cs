e9b4dc78902e431cbb0045e6c6e8305ec0f78735
oneananda <anandathirumurugan@gmail.com>
2025-04-03 14:05:06 +0530

    Adding project for 042_AddSingleton_Deep_Dive

---
+++ 042_AddSingleton_Deep_Dive/Services/TimeService .cs
@@ -0,0 +1,14 @@
+﻿namespace _042_AddSingleton_Deep_Dive.Services
+{
+    public class TimeService : ITimeService
+    {
+        private readonly DateTime _startTime;
+
+        public TimeService()
+        {
+            _startTime = DateTime.Now;
+        }
+
+        public DateTime GetStartTime() => _startTime;
+    }
+}
