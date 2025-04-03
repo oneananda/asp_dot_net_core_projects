29d38e079ac5c23ebd57ef85ea81f4b389676852
oneananda <anandathirumurugan@gmail.com>
2025-04-03 16:30:44 +0530

    AppSettings Model

---
+++ 042_AddSingleton_Deep_Dive/Models/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace _042_AddSingleton_Deep_Dive.Models
+{
+    public class AppSettings
+    {
+        public string AppName { get; set; }
+    }
+}
--- 042_AddSingleton_Deep_Dive/Program.cs
+++ 042_AddSingleton_Deep_Dive/Program.cs
@@ -1,4 +1,5 @@
 
+using _042_AddSingleton_Deep_Dive.Models;
 using _042_AddSingleton_Deep_Dive.Services;
 
 namespace _042_AddSingleton_Deep_Dive
@@ -17,6 +18,13 @@ namespace _042_AddSingleton_Deep_Dive
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            var appSettings = new AppSettings
+            {
+                AppName = "AddSingleton Deep Dive"
+            };
+            builder.Services.AddSingleton(appSettings);
+
 
             var app = builder.Build();
 
