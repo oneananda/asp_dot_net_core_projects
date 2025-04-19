49f2667bcdea28db1c992fb9ed8dad92ac855880
oneananda <anandathirumurugan@gmail.com>
2025-04-19 16:44:31 +0530

    AddTransientHttpErrorPolicy

--- 043_HTTP_ClientFactory_and_Resilient_Calls_With_Polly/Program.cs
+++ 043_HTTP_ClientFactory_and_Resilient_Calls_With_Polly/Program.cs
@@ -1,4 +1,6 @@
 
+using Polly;
+
 namespace _043_HTTP_ClientFactory_and_Resilient_Calls_With_Polly
 {
     public class Program
@@ -13,6 +15,25 @@ namespace _043_HTTP_ClientFactory_and_Resilient_Calls_With_Polly
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddHttpClient("MyApiClient", client =>
+            {
+                client.BaseAddress = new Uri("https://api.example.com/");
+            })
+            // Retry up to 3 times for transient HTTP errors (5xx or network failures):
+            .AddTransientHttpErrorPolicy(policyBuilder =>
+                policyBuilder.WaitAndRetryAsync(
+                    retryCount: 3,
+                    sleepDurationProvider: attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt))
+                )
+            )
+            // Break the circuit for 30 seconds if there are 5 consecutive failures:
+            .AddTransientHttpErrorPolicy(policyBuilder =>
+                policyBuilder.CircuitBreakerAsync(
+                    handledEventsAllowedBeforeBreaking: 5,
+                    durationOfBreak: TimeSpan.FromSeconds(30)
+                )
+            );
 
             var app = builder.Build();
 
