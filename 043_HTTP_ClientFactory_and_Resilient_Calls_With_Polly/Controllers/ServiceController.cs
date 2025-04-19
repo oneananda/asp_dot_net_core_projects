76a370218adfa1922b441def3936ff8d5ce725eb
oneananda <anandathirumurugan@gmail.com>
2025-04-19 16:48:04 +0530

    ServiceController and User model

---
+++ 043_HTTP_ClientFactory_and_Resilient_Calls_With_Polly/Controllers/ServiceController.cs
@@ -0,0 +1,26 @@
+﻿using _043_HTTP_ClientFactory_and_Resilient_Calls_With_Polly.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace _043_HTTP_ClientFactory_and_Resilient_Calls_With_Polly.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        private readonly HttpClient _client;
+        public ServiceController(IHttpClientFactory httpClientFactory)
+        {
+            _client = httpClientFactory.CreateClient("MyApiClient");
+        }
+
+        public async Task<User> GetDataAsync()
+        {
+            // These calls are automatically retried/circuit‑broken as configured
+            var response = await _client.GetAsync("/data");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<User>();
+        }
+    }
+}
---
+++ 043_HTTP_ClientFactory_and_Resilient_Calls_With_Polly/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace _043_HTTP_ClientFactory_and_Resilient_Calls_With_Polly.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
