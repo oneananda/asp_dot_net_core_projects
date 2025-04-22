e4b780cc807212c3ba8d96cb229bbefa87a55bf0
oneananda <anandathirumurugan@gmail.com>
2025-04-22 18:31:27 +0530

    TodoContext

---
+++ 045_Entity_Framework_Core_Performance_Profiling/Data/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using _045_Entity_Framework_Core_Performance_Profiling.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _045_Entity_Framework_Core_Performance_Profiling.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        { }
+
+        public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+    }
+}
