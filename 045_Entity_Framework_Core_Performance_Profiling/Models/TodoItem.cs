189895dc3216393765e4a6c19857a63ae4cfffa3
oneananda <anandathirumurugan@gmail.com>
2025-04-22 18:29:25 +0530

    TodoItem Model

---
+++ 045_Entity_Framework_Core_Performance_Profiling/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace _045_Entity_Framework_Core_Performance_Profiling.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = default!;
+        public bool IsDone { get; set; }
+    }
+}
