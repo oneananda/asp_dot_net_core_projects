b716b43e0b94ffc2bdc6e1455be68b0b0d532219
oneananda <anandathirumurugan@gmail.com>
2025-04-26 12:09:30 +0530

    Adding Entity_Framework_Core_Performance_Profiling_Dashboard project

---
+++ 045_Entity_Framework_Core_Performance_Profiling/Entity_Framework_Core_Performance_Profiling_Dashboard/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Entity_Framework_Core_Performance_Profiling_Dashboard.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
