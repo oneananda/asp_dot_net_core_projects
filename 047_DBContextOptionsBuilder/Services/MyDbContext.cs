553da237b0e6fbec8fb52ab391408732ebfc0316
oneananda <anandathirumurugan@gmail.com>
2025-05-13 18:40:55 +0530

    Using DbContextOptionsBuilder in OnConfiguring

---
+++ 047_DBContextOptionsBuilder/Services/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace _047_DBContextOptionsBuilder.Services
+{
+    public class MyDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=MyDb;Trusted_Connection=True;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
