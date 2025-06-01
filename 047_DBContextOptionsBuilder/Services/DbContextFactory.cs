19f018d6b1e59c1c5c6167d52e9f29c856ae6487
oneananda <anandathirumurugan@gmail.com>
2025-06-01 09:56:29 +0530

    Adding DbContextFactory

---
+++ 047_DBContextOptionsBuilder/Services/DbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _047_DBContextOptionsBuilder.Services
+{
+    public class DbContextFactory : IDbContextFactory
+    {
+        public MyDbContext Create(string connectionString)
+        {
+            var opts = new DbContextOptionsBuilder<MyDbContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+            return new MyDbContext(opts);
+        }
+    }
+}
