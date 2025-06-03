1ddb6c076cb6c73b2d91245f21fbb3d53433e167
oneananda <anandathirumurugan@gmail.com>
2025-06-03 20:32:15 +0530

    Addiitional changes

--- 047_DBContextOptionsBuilder/GrpcServiceTemplate/Services/GreeterService.cs
+++ 047_DBContextOptionsBuilder/GrpcServiceTemplate/Services/GreeterService.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcServiceTemplate;
 
 namespace GrpcServiceTemplate.Services
 {
-    internal class GreeterService
+    public class GreeterService : Greeter.GreeterBase
     {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            var reply = new HelloReply
+            {
+                Message = $"Hello, {request.Name}! Welcome to gRPC."
+            };
+            return Task.FromResult(reply);
+        }
     }
 }
