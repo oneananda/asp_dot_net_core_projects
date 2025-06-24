5c53a11e27a1d3a6d6be170cd3e4a340bc4eda08
oneananda <anandathirumurugan@gmail.com>
2025-06-24 20:13:08 +0530

    Adding models

---
+++ 051_Headless_CMS_With_ASP_Dot_NET_Core/Models/Content.cs
@@ -0,0 +1,14 @@
+﻿namespace _051_Headless_CMS_With_ASP_Dot_NET_Core.Models
+{
+    public class Content
+    {
+        public int Id { get; set; }
+        public string ContentType { get; set; } // e.g., "blog", "page"
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string BodyJson { get; set; } // flexible content structure
+        public bool IsPublished { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+
+}
---
+++ 051_Headless_CMS_With_ASP_Dot_NET_Core/Models/Media.cs
@@ -0,0 +1,11 @@
+﻿namespace _051_Headless_CMS_With_ASP_Dot_NET_Core.Models
+{
+    public class Media
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string Url { get; set; }
+        public string ContentType { get; set; }
+    }
+
+}
