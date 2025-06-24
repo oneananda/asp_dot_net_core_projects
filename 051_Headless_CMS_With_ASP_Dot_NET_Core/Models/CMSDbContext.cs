e3ee8802ecf217de05717891d7eb2131a667ee99
oneananda <anandathirumurugan@gmail.com>
2025-06-24 20:12:45 +0530

    CMSDbContext

---
+++ 051_Headless_CMS_With_ASP_Dot_NET_Core/Models/CMSDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _051_Headless_CMS_With_ASP_Dot_NET_Core.Models
+{
+    public class CMSDbContext : DbContext
+    {
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Media> Media { get; set; }
+
+        public CMSDbContext(DbContextOptions<CMSDbContext> options) : base(options) { }
+    }
+
+}
