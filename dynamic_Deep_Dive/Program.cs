6b39a0c21cbb7f3adef12ec6a8846bffaaab6d1a
oneananda <anandathirumurugan@gmail.com>
2025-06-30 20:20:56 +0530

    Dynamic with primitive operations

--- dynamic_Deep_Dive/Program.cs
+++ dynamic_Deep_Dive/Program.cs
@@ -10,8 +10,15 @@
             Console.WriteLine(name.Length); // Resolved at runtime
 
             // Compare var vs dynamic
+
             var x = "Hello";     // Type resolved at compile time
             dynamic y = "Hello"; // Type resolved at runtime
+
+            // Dynamic with primitive operations
+            
+            dynamic a = 5;
+            dynamic b = 10;
+            Console.WriteLine(a + b); // Runtime resolution
 
 
             Console.WriteLine("Dyanamic Deep Dive!");
