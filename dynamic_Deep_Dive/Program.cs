6968c47b2471464653c5b4fad0be725d44a344a1
oneananda <anandathirumurugan@gmail.com>
2025-06-30 20:19:27 +0530

    dynamic_Deep_Dive

---
+++ dynamic_Deep_Dive/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace dynamic_Deep_Dive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Basic dynamic typing 
+
+            dynamic name = "Alice";
+            Console.WriteLine(name.Length); // Resolved at runtime
+
+            Console.WriteLine("Dyanamic Deep Dive!");
+        }
+    }
+}
